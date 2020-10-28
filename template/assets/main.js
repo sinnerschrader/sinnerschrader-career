0168c02f3a45333bd7758fdde474c660a5cf9ac7
Martin Sonnenholzer <martin.sonnenholzer@sinnerschrader.com>
2020-10-28 15:48:03 +0100

    feat(template): code improvements

--- template/assets/main.js
+++ template/assets/main.js
@@ -1,23 +1,25 @@
-console.log('hi');
 
-const rows = document.querySelectorAll('.jobs-row');
-console.log('rows', rows);
+function addAccordionListener() {
+    const rows = document.querySelectorAll('.jobs-row');
 
-function closeRows(rows) {
+    function closeRows(rows) {
+        rows.forEach(row => {
+            row.classList.remove('open');
+        });
+    }
+
     rows.forEach(row => {
-        row.classList.remove('open');
+       row.addEventListener('click', () => {
+           if(row.classList.contains('open')) {
+               row.classList.remove('open');
+           } else {
+               // close all rows
+               closeRows(rows);
+               // open this one
+               row.classList.add('open');
+           }
+       });
     });
 }
 
-rows.forEach(row => {
-   row.addEventListener('click', () => {
-       if(row.classList.contains('open')) {
-           row.classList.remove('open');
-       } else {
-           // close all rows
-           closeRows(rows);
-           // open this one
-           row.classList.add('open');
-       }
-   });
-});
+addAccordionListener();
